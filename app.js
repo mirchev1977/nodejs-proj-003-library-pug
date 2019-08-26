abab4b4d4401369dd59673d7023c4981d9433317
Miroslav Mirchev <mirchev1977@yahoo.com>
2019-08-26 06:02:12 +0300

    included pug as view engine and printed page 404.pug if no route found

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const libraryRoutes = require( './routes/library' );
 
 const app = express();
 
+app.set( 'view engine', 'pug' );
+app.set( 'views', 'views' );
+
 app.use( '/', libraryRoutes );
+app.use( ( req, res, next ) => {
+    res.render( '404' );
+} );
 
 app.listen( process.mainModule.PORT || 3000 );
