3825478a67ddb51acf615135e2d011ae40cb996c
Miroslav Mirchev <mirchev1977@yahoo.com>
2019-08-26 06:54:01 +0300

    printed all books ont h1 tag

--- routes/library.js
+++ routes/library.js
@@ -1,9 +1,47 @@
 const express = require( 'express' );
+const fs      = require( 'fs'      );
 
 const router = express.Router();
 
 router.get( '/', ( req, res, next )  => {
-    res.render( 'library/allBooks' );
+    getAllBooks( './data/books.txt' ).then( booksArr => {
+        const [ id, title, author, issuedon ] = booksArr.shift().split( ';' );
+        const arrBooks = booksArr.map( bookLine => {
+            const [ bdyId, bdyTitle, bdyAuthor, bdyIssuedOn ] = bookLine.split( ';' );
+            const book = {};
+            book[ id       ] = bdyId;
+            book[ title    ] = bdyTitle;
+            book[ author   ] = bdyAuthor;
+            book[ issuedon ] = bdyIssuedOn;
+
+            if ( bdyId && bdyTitle && bdyAuthor && bdyIssuedOn ) {
+                return book; 
+            }
+        } );
+
+        const arrBooksFiltered = arrBooks.filter( book => {
+            if ( book ) return book;
+        } );
+
+        res.render( 'library/allBooks', { books: arrBooksFiltered } ); 
+    }, err => {
+        debugger;
+    } );
 } );
+
+function getAllBooks ( path ) {
+    const promise = new Promise( ( resolve, reject ) => {
+        let booksStr = '';
+        fs.readFile( path, ( err, data ) => {
+            if ( err ) {
+                reject( err );
+                return;
+            }
+            booksStr += data;
+            resolve( booksStr.split( /\n/ ) );
+        } ); 
+    } );
+    return promise;
+}
 
 module.exports = router;
