e831a8aa1e4ff83d0229401fa045c5ec4303768a
Miroslav Mirchev <mirchev1977@yahoo.com>
2019-08-27 05:17:50 +0300

    created dir public to hold static files

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
+const path    = require( 'path'    );
 const express = require( 'express' );
 
 const libraryRoutes = require( './routes/library' );
 
 const app = express();
 
+
 app.set( 'view engine', 'pug' );
 app.set( 'views', 'views' );
+
+const rootDir = path.dirname( process.mainModule.filename );
+app.use( express.static( path.join( rootDir, 'public' ) ) );
 
 app.use( '/', libraryRoutes );
 app.use( ( req, res, next ) => {
