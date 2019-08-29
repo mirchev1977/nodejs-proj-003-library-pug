b2924252a4e73b0f255eab43b3b1881c8e0819d8
Miroslav Mirchev <mirchev1977@yahoo.com>
2019-08-29 06:58:11 +0300

    page 404.pug - extends ./layouts/main-layout

--- public/scripts/new-book.js
+++ public/scripts/new-book.js
@@ -6,6 +6,8 @@ submitButton.addEventListener( 'submit', ( ev ) => {
     const newBookIssuedOn = document.getElementById( 'new-book__book-issuedon' ).value;
     const errorMessage    = document.getElementById( 'error-message'           );
 
+    errorMessage.style.display = "block";
+
     if ( !newBookTitle || !newBookAuthor || !newBookIssuedOn ) {
         ev.preventDefault();
         errorMessage.innerText = 'All form fields have to be filled!';
