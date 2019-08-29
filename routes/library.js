3bcec402b4d17b9c8868f9c33f6cd4a64fc88a94
Miroslav Mirchev <mirchev1977@yahoo.com>
2019-08-29 07:58:32 +0300

    fetchAllBooks

--- routes/library.js
+++ routes/library.js
@@ -4,32 +4,47 @@ const fs      = require( 'fs'      );
 const router = express.Router();
 
 router.get( '', ( req, res, next )  => {
-    getAllBooks( './data/books.txt' ).then( booksArr => {
-        const [ id, title, author, issuedon ] = booksArr.shift().split( ';' );
-        const arrBooks = booksArr.map( bookLine => {
-            const [ bdyId, bdyTitle, bdyAuthor, bdyIssuedOn ] = bookLine.split( ';' );
-            const book = {};
-            book[ id       ] = bdyId;
-            book[ title    ] = bdyTitle;
-            book[ author   ] = bdyAuthor;
-            book[ issuedon ] = bdyIssuedOn;
-
-            if ( bdyId && bdyTitle && bdyAuthor && bdyIssuedOn ) {
-                return book; 
-            }
-        } );
-
-        const arrBooksFiltered = arrBooks.filter( book => {
-            if ( book ) return book;
-        } );
-
-        res.render( 'library/allBooks', { books: arrBooksFiltered, bookHeaders: {
-            id, title, author, issuedon
-        } } ); 
+    fetchAllBooks ( './data/books.txt' ).then( data => {
+        res.render( 'library/allBooks', { 
+            books:       data[ 'books'       ], 
+            bookHeaders: data[ 'bookHeaders' ] 
+        } ); 
     }, err => {
-        debugger;
+        console.log( err );
     } );
 } );
+
+function fetchAllBooks ( src ) {
+    const promise = new Promise( ( resolve, reject ) => {
+        getAllBooks( src ).then( booksArr => {
+            const [ id, title, author, issuedon ] = booksArr.shift().split( ';' );
+            const arrBooks = booksArr.map( bookLine => {
+                const [ bdyId, bdyTitle, bdyAuthor, bdyIssuedOn ] = bookLine.split( ';' );
+                const book = {};
+                book[ id       ] = bdyId;
+                book[ title    ] = bdyTitle;
+                book[ author   ] = bdyAuthor;
+                book[ issuedon ] = bdyIssuedOn;
+
+                if ( bdyId && bdyTitle && bdyAuthor && bdyIssuedOn ) {
+                    return book; 
+                }
+            } );
+
+            const arrBooksFiltered = arrBooks.filter( book => {
+                if ( book ) return book;
+            } );
+
+            resolve( { books: arrBooksFiltered, bookHeaders: {
+                id, title, author, issuedon 
+            } } );
+        }, err => {
+            debugger;
+        } );
+    } );
+
+    return promise;
+}
 
 function getAllBooks ( path ) {
     const promise = new Promise( ( resolve, reject ) => {
