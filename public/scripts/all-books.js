26ae3b8f00925b6b1d8bdac0ba345fdfc4b230e0
Miroslav Mirchev <mirchev1977@yahoo.com>
2019-08-30 09:00:30 +0300

    fixe delete

--- public/scripts/all-books.js
+++ public/scripts/all-books.js
@@ -2,7 +2,7 @@ Array.from( document.getElementsByClassName( 'delete-book' ) ).forEach(book => {
     book.addEventListener( 'click', ( ev ) => {
         const getPrompt = prompt( 'Delete the current book? Are you sure? Y/[N]' );
 
-        if ( !getPrompt.match( /[yY][eE]?[sS]?/ ) ) {
+        if ( !getPrompt || !getPrompt.match( /[yY][eE]?[sS]?/ ) ) {
             ev.preventDefault();
             return false;
         }
