b245a01f633140d89e63be5123a995809feb994e
Miroslav Mirchev <mirchev1977@yahoo.com>
2019-08-30 08:03:44 +0300

    added https

--- utils/keepAlive.js
+++ utils/keepAlive.js
@@ -1,9 +1,9 @@
-const http  = require( 'http' );
+const http  = require( 'https' );
 
 function keepAlive () {
     clearInterval();
     setInterval( () => {
-        http.get( 'http://localhost:3000', resp => {
+        http.get( 'https://nodejs-proj-003-library-pug.herokuapp.com', resp => {
             //console.log( resp );
         } );
     }, 20 * 60 * 1000 );
